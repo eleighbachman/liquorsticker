1befbcbb3b1e363bfb92042d59cee212db0f206e
Liz Hennessy <eleighhennessy@gmail.com>
2018-09-13 15:10:20 -0600

    adding champagne

--- src/data/index.js
+++ src/data/index.js
@@ -6,7 +6,7 @@ export default [
   },
   {
       name: "Rum & Coke",
-      information: "Rum & Coke ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "As long as you have the rum, fear not!  Your Coke in this case can be subbed out for nearly anything sweet and dark. Choose a root beer, Diet Coke, or even a can of Zevia should your tastebuds request. A straight shot of Kraken rum will take care of the rest.",
       img: "whiskey.png"
 
   },
@@ -17,7 +17,7 @@ export default [
   },
   {
       name: "Vodka Martini",
-      information: "Vodka Martini ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "Remember - it's the vermouth drip around the glass that makes it wet, and the olive juice that makes it dirty. In this rarest of occasions vodka takes the backseat when you ask for it filthy.",
       img: "vodka_martini.png"
   },
   {
@@ -32,17 +32,17 @@ export default [
   },
   {
       name: "White Wine",
-      information: "white wine ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "Elegance. Business. Refinery. Opt for Sauvignon Blanc for a mouth-smacking drought, or go straight for dessert with the best spike to blood sugar a Riesling can offer. Pair with fish, or so I'm told.",
       img: "white_wine.png"
   },
   {
       name: "Gin & Tonic",
-      information: "Cosmopolitan ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "Some like it piney - and some like it full-on coniferous. Watch your proportions of gin to tonic in this sparkling beverage. For those who prefer a taste more delicate than pine needles, partner with a wedge of lime for a twist of sour freshness.",
       img: "gin.png"
   },
   {
       name: "Hefeweizen Beer",
-      information: "Cosmopolitan ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "Plenty of hops and wheat in this masterpiece of German tradition, a golden Hefeweizen is the pinnacle of classic beer. Partner with pretzels, nuts, bratwurst, schnitzel or knödel - and don't forget to say Prost!",
       img: "honey_brew.png"
   },
   {
@@ -52,12 +52,12 @@ export default [
   },
   {
       name: "Irish Coffee",
-      information: "Cosmopolitan ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "Best in the morning, afternoon, evening, or any time that strikes you as Irish. Top with a heavy dollop of whipped cream, cinnamon or chocolate sprinkles to complete this caffeinated delight. Bailey's is the liqueur of choice, with the creamy sweetness needed to counteract a bitter brew.",
       img: "irish_coffee.png"
   },
   {
       name: "Absinthe",
-      information: "Cosmopolitan ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "The envious blood of the devil, and not for the faint of heart. Exercise caution with this potion - it won't come with another disclaimer.",
       img: "absinthe.png"
   },
   {
@@ -68,11 +68,11 @@ export default [
   {
       name: "Champagne",
       information: "Cosmopolitan ipsum dolor sit amet, consectetur adipisicing elit",
-      img: "empty.png"
+      img: "champagne.png"
   },
   {
       name: "Strawberry Daiquiri",
-      information: "Cosmopolitan ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "Is there even alcohol in this? I'll have another, please! Wait - did you drink my daiquiri? Best get a third, I don't feel a thing... who ordered this slurpie for me? Thanks! ...did you take my keys?",
       img: "empty.png"
   },
   {
@@ -97,12 +97,12 @@ export default [
   },
   {
       name: "Beerita",
-      information: "Cosmopolitan ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "Quick. Cold. Crisp. The best of both worlds spun into a cylinder of stylish sloth. Enjoy your beerita in strawberry, mango, watermelon, lime or pineapple - just a walk to the grocery store away!",
       img: "empty.png"
   },
   {
       name: "Bloody Mary",
-      information: "Cosmopolitan ipsum dolor sit amet, consectetur adipisicing elit",
+      information: "Best when complemented with a skewer of asparagus, celery or other veggies, this morning cure-all takes off the edge by adding a smooth finish of its own. Temper your vodka with hot sauce or wasabi and watch the colors fly!",
       img: "empty.png"
   },
   {
